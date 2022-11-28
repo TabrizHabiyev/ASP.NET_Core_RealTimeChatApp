80a9749a737280254ffd510b1e811225ad59c894
Tabriz Habiyev <tabrizmonzo@gmail.com>
2022-11-29 02:09:30 +0400

    Added DbContext Configuration

--- Infrastructure/RealTimeChatApp.Persistance/ServicesRegistration.cs
+++ Infrastructure/RealTimeChatApp.Persistance/ServicesRegistration.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using RealTimeChatApp.Application.UnitOfWork;
+using RealTimeChatApp.Persistance.Contexts;
 
 namespace RealTimeChatApp.Persistance;
 
 public static class ServiceRegistration
 {
-    public static void AddPersistanceServices(this IServiceCollection services)
+    public static IServiceCollection AddPersistanceServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddTransient<IUnitOfWork, UnitOfWork.UnitOfWork>();
+
+        services.AddDbContext<RealTimeChatAppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+            builder => builder.MigrationsAssembly(typeof(RealTimeChatAppDbContext).Assembly.FullName))
+        );
+
+        return services;
     }
 }
--- Presentation/RealTimeChatApp.Presentation/Program.cs
+++ Presentation/RealTimeChatApp.Presentation/Program.cs
@@ -1,4 +1,15 @@
+using RealTimeChatApp.Application;
+using RealTimeChatApp.Infrastructure;
+using RealTimeChatApp.Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices();
+builder.Services.AddPersistanceServices(builder.Configuration);
+
+
 
 // Add services to the container.
 
@@ -10,7 +21,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
