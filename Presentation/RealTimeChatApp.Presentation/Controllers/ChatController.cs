a90792119c57a7bbd9d9dc5f91c52ef32868ab6b
Tabriz Habiyev <tabrizmonzo@gmail.com>
2022-12-03 09:14:15 +0400

    Added  CreateChat , UpdateChat ,  DeleteChat ,GetMessagesByChatId,CreateMessage ,UpdateMessage,DeleteMessage methods  to ChatController

--- Presentation/RealTimeChatApp.Presentation/Controllers/ChatController.cs
+++ Presentation/RealTimeChatApp.Presentation/Controllers/ChatController.cs
@@ -29,5 +29,60 @@ namespace Alfaex.az_API.Controllers
             return Ok(chat);
         }
 
+        [HttpPost]
+        public async Task<ActionResult<ChatDto>> CreateChat(ChatDto chatDto)
+        {
+            var chat = await _chatService.CreateChatAsync(chatDto);
+            return Ok(chat);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ChatDto>> UpdateChat(ChatDto chatDto)
+        {
+            var chat = await _chatService.UpdateChatAsync(chatDto);
+            return Ok(chat);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteChat(Guid id)
+        {
+            await _chatService.DeleteChatAsync(id);
+            return Ok();
+        }
+
+        [HttpGet("{id}/messages")]
+        public async Task<ActionResult<List<ChatMessageDto>>> GetMessagesByChatId(Guid id)
+        {
+            var messages = await _chatService.GetMessagesByChatIdAsync(id);
+            return Ok(messages);
+        }
+
+        [HttpPost("{id}/messages")]
+        public async Task<ActionResult<ChatMessageDto>> CreateMessage(Guid id, ChatMessageDto messageDto)
+        {
+            messageDto.Chat.Id = id;
+            var message = await _chatService.CreateMessageAsync(messageDto);
+            return Ok(message);
+        }
+
+        [HttpPut("{id}/messages")]
+        public async Task<ActionResult<ChatMessageDto>> UpdateMessage(Guid id, ChatMessageDto messageDto)
+        {
+            messageDto.Chat.Id = id;
+            var message = await _chatService.UpdateMessageAsync(messageDto);
+            return Ok(message);
+        }
+
+        [HttpDelete("{id}/messages")]
+        public async Task<ActionResult> DeleteMessage(Guid id)
+        {
+            await _chatService.DeleteMessageAsync(id);
+            return Ok();
+        }
+
+
+
+
+
     }
 }
