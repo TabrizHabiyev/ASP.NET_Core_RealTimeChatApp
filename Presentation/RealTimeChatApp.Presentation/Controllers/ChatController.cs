58252c7c076b9e5314c2009a7f77178b9a845027
Tabriz Habiyev <tabrizmonzo@gmail.com>
2022-12-03 09:08:36 +0400

    added  GetAllChats method to ChatController

--- Presentation/RealTimeChatApp.Presentation/Controllers/ChatController.cs
+++ Presentation/RealTimeChatApp.Presentation/Controllers/ChatController.cs
@@ -1,60 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using RealTimeChatApp.Application.DTOs.User;
-using RealTimeChatApp.Application.UnitOfWork;
+using RealTimeChatApp.Application.Common.Interfaces.Services;
+using RealTimeChatApp.Application.DTOs.Chat;
 using RealTimeChatApp.Presentation.Helpers;
 
 namespace Alfaex.az_API.Controllers
 {
     [ApiController]
     [Route("api/[controller]/[action]")]
-    public class AuthController : BaseControllerHelper
+    public class ChatController : BaseControllerHelper
     {
-        private readonly IUnitOfWork _unitOfWork;
-
-        public AuthController(IUnitOfWork unitOfWork)
+        private readonly IChatService _chatService;
+        public ChatController(IChatService chatService)
         {
-            _unitOfWork = unitOfWork;
+            _chatService = chatService;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginUserRequestDto loginUserRequestDto)
+        [HttpGet]
+        public async Task<ActionResult<List<ChatDto>>> GetAllChats()
         {
-            if (ModelState.IsValid) { }
-            var result = await _unitOfWork.AuthService.LoginAsync(loginUserRequestDto);
-            return Ok(result);
+            var chats = await _chatService.GetAllChatsAsync();
+            return Ok(chats);
         }
 
-
-        [HttpPost]
-        public async Task<IActionResult> Register([FromForm] CreateUserDto createUserDto)
-        {
-            if (ModelState.IsValid) { }
-            var result = await _unitOfWork.UserService.CreateAsync(createUserDto);
-            return Ok(result);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> PasswordReset([FromForm] ResetUserPasswordRequestDto resetUserPasswordRequestDto)
-        {
-            if (ModelState.IsValid) { }
-            var result = await _unitOfWork.AuthService.PasswordResetdByEmailAsync(resetUserPasswordRequestDto);
-            return Ok(result);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UpdatePassword([FromForm] UpdateUserPasswordRequestDto updateUserPasswordRequestDto)
-        {
-            if (ModelState.IsValid) { }
-            var result = await _unitOfWork.AuthService.UpdateUserPassword(updateUserPasswordRequestDto);
-            return Ok(result);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> ConfirimEmail([FromForm] ConfirmUserEmailRequestDto confirmUserEmailRequestDto)
-        {
-            if (ModelState.IsValid) { }
-            var result = await _unitOfWork.AuthService.ConfirmEmail(confirmUserEmailRequestDto);
-            return Ok(result);
-        }
     }
 }
